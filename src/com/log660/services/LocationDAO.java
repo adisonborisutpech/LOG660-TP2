9d7cccb64f097b511f75991873d5568aede62d9d
Unknown <zasorick@gmail.com>
2018-06-20 15:18:49 -0400

    Added DAO for location

--- src/com/log660/services/LocationDAO.java
+++ src/com/log660/services/LocationDAO.java
@@ -1,5 +1,56 @@
 package com.log660.services;
 
-public class LocationDAO {
+import java.util.Iterator;
+import java.util.List;
 
+import com.log660.beans.Locationfilm;
+import com.log660.utils.HibernateUtil;
+
+public class LocationDAO {
+	
+	public static List<Locationfilm> getLocationByPersonneId(int personneGUID) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		Transaction trans = null;
+		List<Locationfilm> locations = null;
+		try {
+			
+			trans = session.beginTransaction();		
+			locations = (List<Locationfilm>)session.createCriteria(Locationfilm.class).add(Restrictions.eq("noUtilisateur", personneGUID));
+			System.out.println(personneGUID);
+			trans.commit();
+			
+		} catch (HibernateException e) {
+			trans.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}	
+		
+		return locations;
+	}
+	
+	public static List<Locationfilm> getLocationByFilmId(int filmGUID) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		Transaction trans = null;
+		List<Locationfilm> locations = null;
+		try {
+			
+			trans = session.beginTransaction();		
+			locations = (List<Locationfilm>)session.createCriteria(Locationfilm.class).add(Restrictions.eq("noFilm", filmGUID));
+			System.out.println(filmGUID);
+			trans.commit();
+			
+		} catch (HibernateException e) {
+			trans.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}	
+		
+		return locations;
+	}
 }
