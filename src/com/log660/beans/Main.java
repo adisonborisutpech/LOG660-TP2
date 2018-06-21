cba643a1ad0f605880c72e0ad1cc065cfd3963d6
Adison Borisutpech <adisonborisutpech@gmail.com>
2018-06-20 22:13:23 -0400

    Added test for searchMovie on another main

--- src/com/log660/beans/Main.java
+++ src/com/log660/beans/Main.java
@@ -1,5 +1,6 @@
 package com.log660.beans;
 
+import com.log660.controllers.Controller;
 import com.log660.services.FilmDAO;
 import com.log660.services.LocationDAO;
 import com.log660.services.PersonneDAO;
@@ -9,11 +10,13 @@ public class Main {
 	public static void main(String[] args) {
 		//UtilisateurDAO.getUtilisateurByEmail("PeterFBorja24@hotmail.com");
 		//PersonneDAO.getPersonneById(833865);
-		String[] test = {"USA"};
-		String[] test1 = {"Family"};
+		String[] test = {};
+		String[] test1 = {};
+		String[] test4 = {};
 		String[] test2 = {};
-		String[] test3 = {"Leslie Mann"};
-		//FilmDAO.getFilmByCriteria("", 1997, 1997, test, "", test1, test2, test3);
-		LocationDAO.getLocationByUtilisateurId(441887);
+		String[] test3 = {};
+		//FilmDAO.getFilmByCriteria("", 1997, 1997, test, test4, test1, test2, test3);
+		Controller.searchMovie("", 1997, 1997, test, test4, test1, test2, test3);
+		//LocationDAO.getLocationByUtilisateurId(441887);
 	}
 }
